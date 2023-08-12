70dae027413396336ef42dd6683e6b622acb1233
CodedSakura <sakuracoded@gmail.com>
2023-08-12 22:14:01 +0200

    feat: implement tar and tar.gz compression

--- internal/compression/gzipCompressor.go
+++ internal/compression/gzipCompressor.go
@@ -1,10 +1,24 @@
 package compression
 
+import (
+	"WebCompressor/internal/configuration"
+	"archive/tar"
+	"compress/gzip"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
 type GZipCompressor struct {
+	config *configuration.Configuration
 }
 
-func NewGZipCompressor() *GZipCompressor {
-	return &GZipCompressor{}
+func NewGZipCompressor(config *configuration.Configuration) *GZipCompressor {
+	return &GZipCompressor{config: config}
 }
 
 func (c *GZipCompressor) Mimetype() string {
@@ -14,6 +28,66 @@ func (c *GZipCompressor) Extension() string {
 	return "tar.gz"
 }
 func (c *GZipCompressor) Compress(targetPath string) (*State, error) {
-	// placeholder
-	return newState(c), nil
+	state := newState(c)
+
+	go func() {
+		// https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
+		file, err := os.Create(state.Path)
+		if err != nil {
+			state.Progress = -1
+			state.FinishedTime = time.Now()
+			return
+		}
+		defer file.Close()
+
+		g := gzip.NewWriter(file)
+		defer g.Close()
+
+		w := tar.NewWriter(g)
+		defer w.Close()
+
+		tarRootPath := path.Join(c.config.RootPath, targetPath)
+		err = filepath.Walk(
+			tarRootPath,
+			func(path string, info fs.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if !info.Mode().IsRegular() {
+					return nil
+				}
+
+				header, err := tar.FileInfoHeader(info, info.Name())
+				if err != nil {
+					return err
+				}
+
+				trimmedPath := strings.TrimPrefix(path, tarRootPath)
+				trimmedPath = strings.TrimPrefix(trimmedPath, "/")
+				header.Name = trimmedPath
+
+				err = w.WriteHeader(header)
+				if err != nil {
+					return err
+				}
+
+				file, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+
+				_, err = io.Copy(w, file)
+				return err
+			},
+		)
+		state.Progress = 1
+		state.FinishedTime = time.Now()
+		if err != nil {
+			state.Progress = -1
+			return
+		}
+	}()
+
+	return state, nil
 }
--- internal/compression/tarCompressor.go
+++ internal/compression/tarCompressor.go
@@ -1,10 +1,23 @@
 package compression
 
+import (
+	"WebCompressor/internal/configuration"
+	"archive/tar"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
 type TarCompressor struct {
+	config *configuration.Configuration
 }
 
-func NewTarCompressor() *TarCompressor {
-	return &TarCompressor{}
+func NewTarCompressor(config *configuration.Configuration) *TarCompressor {
+	return &TarCompressor{config: config}
 }
 
 func (c *TarCompressor) Mimetype() string {
@@ -14,6 +27,63 @@ func (c *TarCompressor) Extension() string {
 	return "tar"
 }
 func (c *TarCompressor) Compress(targetPath string) (*State, error) {
-	// placeholder
-	return newState(c), nil
+	state := newState(c)
+
+	go func() {
+		// https://medium.com/@skdomino/taring-untaring-files-in-go-6b07cf56bc07
+		file, err := os.Create(state.Path)
+		if err != nil {
+			state.Progress = -1
+			state.FinishedTime = time.Now()
+			return
+		}
+		defer file.Close()
+
+		w := tar.NewWriter(file)
+		defer w.Close()
+
+		tarRootPath := path.Join(c.config.RootPath, targetPath)
+		err = filepath.Walk(
+			tarRootPath,
+			func(path string, info fs.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				if !info.Mode().IsRegular() {
+					return nil
+				}
+
+				header, err := tar.FileInfoHeader(info, info.Name())
+				if err != nil {
+					return err
+				}
+
+				trimmedPath := strings.TrimPrefix(path, tarRootPath)
+				trimmedPath = strings.TrimPrefix(trimmedPath, "/")
+				header.Name = trimmedPath
+
+				err = w.WriteHeader(header)
+				if err != nil {
+					return err
+				}
+
+				file, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+
+				_, err = io.Copy(w, file)
+				return err
+			},
+		)
+		state.Progress = 1
+		state.FinishedTime = time.Now()
+		if err != nil {
+			state.Progress = -1
+			return
+		}
+	}()
+
+	return state, nil
 }
